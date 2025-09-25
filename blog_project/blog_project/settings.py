e9492ac699d599b97fcbf5b4a2dd0f498ade20ce
John Wroge <wrogejohn@gmail.com>
2025-09-25 10:19:13 -0400

    Remove settings file

--- blog_project/blog_project/settings.py
+++ blog_project/blog_project/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-_5$vjr-o9pjknq$!i@zq)0ddp42v1_vmp!ni3s6twfq=n=qa1c')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True').lower() in ('true', '1', 'yes')
